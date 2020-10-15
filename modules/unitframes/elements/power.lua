699d0e66a29b9fad2781c5cf8baf4d876778f6d5
Nick <git@bockmeulen.nl>
2020-10-15 22:08:27 +0200

    wip

--- modules/unitframes/elements/power.lua
+++ modules/unitframes/elements/power.lua
@@ -1,12 +1,16 @@
 local E, L, V, P, G = unpack(ElvUI);
 local UF = E:GetModule('UnitFrames')
 
+local UnitGroupRolesAssigned = UnitGroupRolesAssigned
+local UnitName = UnitName
+
+
 function UF:Configure_Power(frame)
 	local power = frame.Power
 	if not power then return end
     
-    local role = UnitGroupRolesAssigned(frame)
-    print(power)
+    local role = UnitGroupRolesAssigned(player)
+    print(role)
         if role ~= "HEALER" then return end
         print("healer gevonden")
         power.hide()
